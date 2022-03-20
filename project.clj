ba6855aa951b3d55881ec1da6c336e4a30862afa
Sergey Zaborovsky <git@seryiza.xyz>
2022-03-20 15:40:15 +1100

    Add migratus to project

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject health-patient "0.1.0"
   :description "Web service to work with patients records"
   :url "https://github.com/Seryiza/health-patient"
-  :dependencies [[org.clojure/clojure "1.10.3"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.postgresql/postgresql "42.3.1"]
+                 [migratus "1.3.6"]]
+  :plugins [[migratus-lein "0.7.3"]]
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db (System/getenv "DATABASE_URL")})
