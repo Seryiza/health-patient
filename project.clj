74c082431b1de3c96b50e236a8a4549b0aac419e
Sergey Zaborovsky <git@seryiza.xyz>
2022-03-20 14:44:00 +1100

    Add project template with gitignore

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject health-patient "0.1.0"
+  :description "Web service to work with patients records"
+  :url "https://github.com/Seryiza/health-patient"
+  :dependencies [[org.clojure/clojure "1.10.3"]])
