29c708f7e27a5a06e34d848dad69161ba51cc91f
Sergey Zaborovsky <git@seryiza.xyz>
2022-03-20 16:50:45 +1100

    Add mount, hugsql and hikari-cp

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/Seryiza/health-patient"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.postgresql/postgresql "42.3.1"]
-                 [migratus "1.3.6"]]
+                 [migratus "1.3.6"]
+                 [com.layerware/hugsql "0.5.1"]
+                 [hikari-cp "2.13.0"]
+                 [mount "0.1.16"]]
   :plugins [[migratus-lein "0.7.3"]]
   :migratus {:store :database
              :migration-dir "migrations"
