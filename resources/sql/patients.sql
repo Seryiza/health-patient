069cb6156547f52330a265290c5d8420f67441da
Sergey Zaborovsky <git@seryiza.xyz>
2022-03-29 22:17:36 +1100

    Replace hugsql values by more human readable ones

--- resources/sql/patients.sql
+++ resources/sql/patients.sql
@@ -1,21 +1,21 @@
--- :name all-patients :? :*
+-- :name all-patients :query :many
 -- :doc Get all patients
 SELECT * FROM patients
 WHERE is_deleted = false
 ORDER BY id;
 
--- :name patient-by-id :? :1
+-- :name patient-by-id :query :one
 -- :doc Get one patient by id
 SELECT * FROM patients
 WHERE id = :id AND is_deleted = false;
 
--- :name delete-patient-by-id :! :n
+-- :name delete-patient-by-id :execute :affected
 -- :doc Delete one patient by id
 UPDATE patients
 SET is_deleted = true
 WHERE id = :id AND is_deleted = false;
 
--- :name update-patient-by-id :! :n
+-- :name update-patient-by-id :execute :affected
 -- :doc Update one patient by id
 UPDATE patients
 SET first_name = :first_name,
