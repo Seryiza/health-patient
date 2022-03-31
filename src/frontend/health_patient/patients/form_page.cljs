bc3229775be2d1653a183b8e5275de69f2839b3e
Sergey Zaborovsky <git@seryiza.xyz>
2022-03-31 22:31:39 +1100

    Extract common form functions to frontend/utils

--- src/frontend/health_patient/patients/form_page.cljs
+++ src/frontend/health_patient/patients/form_page.cljs
@@ -17,40 +17,20 @@
 (defn get-form-patient-id [form]
   (dommy/attr form :data-patient-id))
 
-(defn get-field-input [form input-name]
-  (dommy/sel1 form (utils/selector-by-name input-name)))
-
-(defn get-field-error [form input-name]
-  (dommy/sel1 form (str "[data-error-for='" (name input-name) "']")))
-
-(defn clear-form-errors [form]
-  (doseq [field-input (dommy/sel form (str "[aria-invalid='true']"))]
-    (dommy/set-attr! field-input :aria-invalid ""))
-  (doseq [field-error (dommy/sel form "[data-error-for]")]
-    (dommy/set-text! field-error "")))
-
-(defn show-form-errors [form errors]
-  (clear-form-errors form)
-  (doseq [[input-name error-msg] errors]
-    (when-let [field-input (get-field-input form input-name)]
-      (when-let [field-error (get-field-error form input-name)]
-        (dommy/set-attr! field-input :aria-invalid "true")
-        (dommy/set-text! field-error error-msg)))))
-
 (defn try-save-patient! [form]
   (let [patient-id (get-form-patient-id form)
         form-data (utils/get-form-values form patient-input-names)
         [validation-errors patient-data] (st/validate form-data schemes/+patient-scheme+)]
     (if (empty? validation-errors)
       (patients-api/save-patient! patient-id patient-data)
-      (show-form-errors form validation-errors))))
+      (utils/show-form-errors form validation-errors))))
 
 (defn try-create-patient! [form]
   (let [form-data (utils/get-form-values form patient-input-names)
         [validation-errors patient-data] (st/validate form-data schemes/+patient-scheme+)]
     (if (empty? validation-errors)
       (patients-api/create-patient! patient-data)
-      (show-form-errors form validation-errors))))
+      (utils/show-form-errors form validation-errors))))
 
 (defn init []
   (doseq [form (dommy/sel "[data-save-patient-form]")]
--- src/frontend/health_patient/utils.cljs
+++ src/frontend/health_patient/utils.cljs
@@ -23,3 +23,23 @@
   (->> input-names
        (map #(hash-map % (get-input-value form %)))
        (apply merge)))
+
+(defn get-field-input [form input-name]
+  (dommy/sel1 form (selector-by-name input-name)))
+
+(defn get-field-error [form input-name]
+  (dommy/sel1 form (str "[data-error-for='" (name input-name) "']")))
+
+(defn clear-form-errors [form]
+  (doseq [field-input (dommy/sel form (str "[aria-invalid='true']"))]
+    (dommy/set-attr! field-input :aria-invalid ""))
+  (doseq [field-error (dommy/sel form "[data-error-for]")]
+    (dommy/set-text! field-error "")))
+
+(defn show-form-errors [form errors]
+  (clear-form-errors form)
+  (doseq [[input-name error-msg] errors]
+    (when-let [field-input (get-field-input form input-name)]
+      (when-let [field-error (get-field-error form input-name)]
+        (dommy/set-attr! field-input :aria-invalid "true")
+        (dommy/set-text! field-error error-msg)))))
