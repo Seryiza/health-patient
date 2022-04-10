f717b217b88059e7d50b2cbcea3f953155daec93
Sergey Zaborovsky <git@seryiza.xyz>
2022-04-10 21:06:23 +1100

    Move setting of jdbc default opts to honey-* functions

--- src/backend/health_patient/db.clj
+++ src/backend/health_patient/db.clj
@@ -8,16 +8,23 @@
   (:import com.zaxxer.hikari.HikariDataSource))
 
 (defstate db
-  :start (-> (jdbc-conn/->pool HikariDataSource {:jdbcUrl (:database-jdbc-url config)
-                                                 :maximumPoolSize 15})
-             (jdbc/with-options {:builder-fn rs/as-unqualified-maps}))
+  :start (jdbc-conn/->pool HikariDataSource {:jdbcUrl (:database-jdbc-url config)
+                                             :maximumPoolSize 15})
   :stop (.close db))
 
-(defn honey-execute! [db statement & [opts]]
-  (jdbc/execute! db (sql/format statement) opts))
+(def default-opts {:builder-fn rs/as-unqualified-maps})
 
-(defn honey-execute-one! [db statement & [opts]]
-  (jdbc/execute-one! db (sql/format statement) opts))
+(defn honey-execute!
+  ([db statement]
+   (honey-execute! db statement default-opts))
+  ([db statement opts]
+   (jdbc/execute! db (sql/format statement) opts)))
+
+(defn honey-execute-one!
+  ([db statement]
+   (honey-execute-one! db statement default-opts))
+  ([db statement opts]
+   (jdbc/execute-one! db (sql/format statement) opts)))
 
 (defn honey-update! [db statement & [opts]]
   (:next.jdbc/update-count (honey-execute-one! db statement opts)))
